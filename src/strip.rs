024258952a57b2dcc510f2946974fcee96484ca2
Morgan Nesbitt <captainmorgan@Morgans-iMac.local>
2019-07-13 18:18:41 -0600

    working iterate rows and columns and save

--- src/strip.rs
+++ src/strip.rs
@@ -39,6 +39,83 @@ pub fn strip_transparency() {
     let image = image::open(file_path).expect("Expect image to load");
     let workable_image = image.as_rgba8().expect("Expect to be convertable to rgba8");
 
+    let (columns, rows) = workable_image.dimensions();
+
+    let mut top_row = (false, 0);
+    let mut bottom_row = (false, 0);
+    let mut left_column = (false, 0);
+    let mut right_column = (false, 0);
+
+    let mut raw_pixels: Vec<(u32, u32, &image::Rgba<u8>)> = workable_image.enumerate_pixels().collect();
+
+    for (_, y, Rgba { data: [_, _, _, alpha] }) in &raw_pixels {
+        if top_row.0 == false && alpha > &0 {
+            top_row = (true, *y);
+            break;
+        }
+    }
+
+    for x in 0..columns {
+        for y in 0..rows {
+            let index: usize = ((y * columns) + x) as usize;
+            let (_, _, Rgba { data: [_, _, _, alpha] }) = raw_pixels[index];
+            if left_column.0 == false && alpha > &0 {
+                left_column = (true, x);
+                break;
+            }
+        }
+
+        if left_column.0 == true {
+            break;
+        }
+    }
+
+    for x in (0..columns).rev() {
+        for y in 0..rows {
+            let index: usize = ((y * columns) + x) as usize;
+            let (_, _, Rgba { data: [_, _, _, alpha] }) = raw_pixels[index];
+            if right_column.0 == false && alpha > &0 {
+                right_column = (true, x);
+                break;
+            }
+        }
+
+        if right_column.0 == true {
+            break;
+        }
+    }
+
+    raw_pixels.reverse();
+
+    for (_, y, Rgba { data: [_, _, _, alpha] }) in &raw_pixels {
+        if bottom_row.0 == false && alpha > &0 {
+            bottom_row = (true, *y);
+            break;
+        }
+    }
+
+    println!("top {:?}, bottom {:?}, left {:?}, right {:?}", top_row, bottom_row, left_column, right_column);
+
+    let output_file_path = "./src/resources/Stripped/Desert/planks_W.png";
+
+    let x = left_column.1;
+    let y = top_row.1;
+    let width = right_column.1 - left_column.1;
+    let height = bottom_row.1 - top_row.1;
+
+    let sub_image = image::SubImage::new(workable_image, x, y, width + 1, height + 1);
+    let bounded_image = sub_image.to_image();
+
+    bounded_image.save(output_file_path).expect("Expected to save sub image");
+}
+
+
+fn unused() {
+    let file_path = "./src/resources/Desert/planks_W.png";
+
+    let image = image::open(file_path).expect("Expect image to load");
+    let workable_image = image.as_rgba8().expect("Expect to be convertable to rgba8");
+
     // TODO validate bounding boxes against image dimension
     // maybe clamp
 
